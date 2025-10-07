d7f039e0e022f80741b6c37c103a1557b5867423
Kobliod <m.davies0109@gmail.com>
2025-10-07 15:21:01 +0100

    Pawns can no longer leave the board

--- chessington/engine/pieces.py
+++ chessington/engine/pieces.py
@@ -39,29 +39,47 @@ class Pawn(Piece):
     """
     A class representing a chess pawn.
     """
+    # board coordinates assumed 0..7 (standard 8x8)
+    BOARD_MIN = 0
+    BOARD_MAX = 7
+
+    def _in_bounds(self, row: int, col: int) -> bool:
+        return self.BOARD_MIN <= row <= self.BOARD_MAX and self.BOARD_MIN <= col <= self.BOARD_MAX
+
     def get_available_moves(self, board) -> List[Square]:
         current_square = board.find_piece(self)
         moves = []
 
         if self.player == Player.BLACK:
-            one_step = Square.at(current_square.row - 1, current_square.col)
-            if board.get_piece(one_step) is None:
-                moves.append(one_step)
-            
-                if current_square.row == 6:
-                    two_step = (Square.at(current_square.row - 2, current_square.col))
-                    if board.get_piece(two_step) is None:
-                        moves.append(two_step)
+            # one step forward
+            r1, c = current_square.row - 1, current_square.col
+            if self._in_bounds(r1, c):
+                one_step = Square.at(r1, c)
+                if board.get_piece(one_step) is None:
+                    moves.append(one_step)
 
-        else:
-            one_step = Square.at(current_square.row + 1, current_square.col)
-            if board.get_piece(one_step) is None:
-                moves.append(one_step)
-            
-                if current_square.row == 1:
-                    two_step = (Square.at(current_square.row + 2, current_square.col))
-                    if board.get_piece(two_step) is None:
-                        moves.append(two_step)
+                    # two-step from starting row (6 for black)
+                    if current_square.row == 6:
+                        r2 = current_square.row - 2
+                        if self._in_bounds(r2, c):
+                            two_step = Square.at(r2, c)
+                            if board.get_piece(two_step) is None:
+                                moves.append(two_step)
+
+        else:  # Player.WHITE
+            r1, c = current_square.row + 1, current_square.col
+            if self._in_bounds(r1, c):
+                one_step = Square.at(r1, c)
+                if board.get_piece(one_step) is None:
+                    moves.append(one_step)
+
+                    # two-step from starting row (1 for white)
+                    if current_square.row == 1:
+                        r2 = current_square.row + 2
+                        if self._in_bounds(r2, c):
+                            two_step = Square.at(r2, c)
+                            if board.get_piece(two_step) is None:
+                                moves.append(two_step)
 
         return moves
 
